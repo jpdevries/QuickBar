88bc02a53353ce3755b81e486103de243d153186
JP DeVries <johnpaul.devries@gmail.com>
2013-08-08 22:59:04 -0700

    Adding New Themes

---
+++ core/components/quickbar/lexicon/en/default.inc.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Default Language file for quickbar
+ *
+ * @package quickbar
+ * @subpackage lexicon
+ */
+
+$_lang['setting_quickbar'] = 'QuickBar';
+
+$_lang['setting_quickbar.css'] = 'CSS';
+$_lang['setting_quickbar.css_desc'] = 'Absolute path to theme CSS file. Value is parsed so MODX tags like [[++assets_url]] can be used.';
+
+$_lang['setting_quickbar.create'] = 'Create';
+$_lang['setting_quickbar.create_desc'] = 'If enabled and user has permissions, displays a create here button in the toolbar.';
+
+$_lang['setting_quickbar.dashboard'] = 'Dashboard';
+$_lang['setting_quickbar.dashboard_desc'] = 'If enabled, displays a dashboard here button in the toolbar.';
+
+$_lang['setting_quickbar.help'] = 'Help';
+$_lang['setting_quickbar.help_desc'] = 'If enabled, displays a help button in the toolbar.';
+
+$_lang['setting_quickbar.helplink'] = 'Help Link';
+$_lang['setting_quickbar.helplink_desc'] = 'URL to use for help button.';
+
+$_lang['setting_quickbar.helptarget'] = 'Help Target';
+$_lang['setting_quickbar.helptarget_desc'] = 'Link target to use for help button.';
