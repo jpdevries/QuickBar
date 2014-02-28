87b4c05c2a2be6f5104993bbf15d63b2b3d65795
JP DeVries <johnpaul.devries@gmail.com>
2014-02-28 13:05:33 -0800

    Refactoring authentication

--- core/components/quickbar/elements/snippets/snippet.quickbar.php
+++ core/components/quickbar/elements/snippets/snippet.quickbar.php
@@ -1,18 +1,5 @@
 <?php
-if (!$modx->user->hasSessionContext('mgr') ) return '';
-
-$resource = $modx->resource;
-$user = $modx->getUser();
-$membership = $user->getUserGroups();
-     
-$policies = $resource->findPolicy();
-     
-if(count($policies)){
-    $checkPolicy = $this->hasAccess($policies,$membership);        
-    if(!$checkPolicy){
-        return '{"error":"Access is denied"}';
-    }
-}
+if (!$modx->user->hasSessionContext('mgr') || !$modx->hasPermission('edit_resource') ) return '';
 
 $defaultQuickBarCorePath = $modx->getOption('core_path').'components/quickbar/';
 $quickbarsCorePath = $modx->getOption('quickbar.core_path',null,$defaultQuickBarCorePath);
