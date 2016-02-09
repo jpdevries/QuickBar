2fb706955625948ace5e8c46907b7a07174e0a81
Susan Ottwell <sottwell@sottwell.com>
2016-02-09 17:55:42 +0200

    Update snippet.quickbar.php
    Added snippet property "tpl" to assign a chunk to use

--- core/components/quickbar/elements/snippets/snippet.quickbar.php
+++ core/components/quickbar/elements/snippets/snippet.quickbar.php
@@ -1,9 +1,12 @@
 <?php
 if (!$modx->user->hasSessionContext('mgr') || !$modx->hasPermission('edit_document') ) return '';
 
+// get the snippet property for the chunk tpl
+$qbTpl = $modx->getOption('tpl', $scriptProperties, 'quickbar');
+
 $defaultQuickBarCorePath = $modx->getOption('core_path').'components/quickbar/';
 $quickbarsCorePath = $modx->getOption('quickbar.core_path',null,$defaultQuickBarCorePath);
 $quickbar = $modx->getService('quickbar','QuickBar',$quickbarsCorePath.'model/quickbar/',$scriptProperties);
 
 $modx->regClientCSS($quickbar->parse($modx->getOption('quickbar.css',null,$modx->getOption('assets_url') . 'components/quickbar/css/retro.css')));
-return $quickbar->getChunk('quickbar',array('mgr_url' => MODX_MANAGER_URL));
+return $quickbar->getChunk($qbTpl,array('mgr_url' => MODX_MANAGER_URL));
